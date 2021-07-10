092cdfc124611d96cc72ff975a7a347a838612b8
amalia <amaliarasyid1999@gmail.com>
2021-07-10 17:35:32 +0700

    HANDLING ERROR - tambah helper untuk mendapatkan pesan berdasarkan status kodenya.

--- api/Disease/read.php
+++ api/Disease/read.php
@@ -2,10 +2,10 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-// include database and object files
-
+// include database,helper, and object files
 include_once '../../config/DbConnection.php';
 include_once '../../model/Disease.php';
+include_once '../../helper/HttpResponseMessage.php';
 
 // instantiate database
 $dbConn = new DbConnection();
@@ -18,34 +18,44 @@ $diseases = new Disease($db);
 $stmt = $diseases->getDiseases();
 $itemCount = $stmt->rowCount();
 
-// echo json_encode($stmt);
 
-//DATA
+//response
 if($itemCount > 0){
-    $response = array(
-        'code' => '200',
-        'message' => 'Data Penyakit berhasil diperoleh',
-        'result' => array()
-    );
-    
-    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
-        extract($row);
-        $item = array(
-            'id_penyakit' => $id_penyakit,
-            'kd_penyakit' => $kd_penyakit,
-            'nm_penyakit' => $nm_penyakit,
-            'definisi' => $definisi
+    if(http_response_code() == 200){
+        $response = array(
+            'code' => '200',
+            'message' => 'Data Penyakit berhasil diperoleh',
+            'result' => array()
         );
-        // var_dump($item);
-        array_push($response['result'],$item);
-    }
-    echo json_encode($response);
-}else{
-    http_response_code(404);
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $item = array(
+                'id_penyakit' => $id_penyakit,
+                'kd_penyakit' => $kd_penyakit,
+                'nm_penyakit' => $nm_penyakit,
+                'definisi' => $definisi
+            );
+            // var_dump($item);
+            array_push($response['result'],$item);
+        }
+        echo json_encode($response);
+    }else{
+        echo json_encode(
+            array(
+                "message" => getHttpMessage(http_response_code()),
+                'code' => http_response_code(),
+                'result' => array()
+            )
+        );
+    }     
+}
+else{
+    http_response_code(204);
     echo json_encode(
         array(
-            "message" => "Data Penyakit tidak ditemukan!",
-            'code' => "404",
+            "message" => "Data Penyakit kosong!",
+            'code' => "204",
             'result' => array()
         )
     );
