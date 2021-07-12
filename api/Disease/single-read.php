ee4b83e853e2ba9b52d3aa134d252ef3efbe17ba
amalia <amaliarasyid1999@gmail.com>
2021-07-12 15:36:46 +0700

    ADD - fitur get penyakit berdasarkan id-nya

---
+++ api/Disease/single-read.php
@@ -0,0 +1,68 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// include database,helper, and object files
+include_once '../../config/DbConnection.php';
+include_once '../../model/Disease.php';
+include_once '../../helper/HttpResponseMessage.php';
+
+// instantiate database
+$dbConn = new DbConnection();
+$db = $dbConn->getConnection();
+
+// initialize object
+$disease = new Disease($db);
+
+//input
+$idPenyakit = isset($_POST['id_penyakit']) ? $_POST['id_penyakit'] :  0 ;
+// var_dump($_POST['id_penyakit']);
+if($idPenyakit == 0){
+    echo json_encode(
+        array(
+            'message' => "input id_penyakit tidak boleh kosong"
+        )
+    );
+    http_response_code(404);
+    return;
+}
+
+//query
+$stmt = $disease->getDiseasesById($idPenyakit);
+$itemCount = $stmt->rowCount();
+
+
+//response
+if($itemCount > 0){
+    if(http_response_code() == 200){
+        $response = array(
+            'code' => 200,
+            'message' => 'Data Penyakit berhasil diperoleh',
+            'result' => new stdClass
+        );
+
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            extract($row);
+            $disease->init($id_penyakit,$kd_penyakit,$nm_penyakit,$definisi);
+            // var_dump($item);
+             $response['result']=$disease;
+        }
+        echo json_encode($response);
+    }else{
+        echo json_encode(
+            array(
+                "message" => getHttpMessage(http_response_code()),
+                'code' => http_response_code()
+            )
+        );
+    }     
+}
+else{
+    http_response_code(404);
+    echo json_encode(
+        array(
+            "message" => "Data Penyakit tidak ditemukan!",
+            'code' => 404
+        )
+    );
+}
--- model/Disease.php
+++ model/Disease.php
@@ -25,7 +25,14 @@ class Disease{
     }
 
     public function getDiseases(){
-        $query = "SELECT id_penyakit, kd_penyakit, nm_penyakit, definisi FROM cbr_penyakit";
+        $query = "SELECT penyakit.* FROM cbr_penyakit as penyakit";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function getDiseasesById($id_penyakit){
+        $query = "SELECT penyakit.* FROM cbr_penyakit as penyakit WHERE penyakit.id_penyakit = $id_penyakit";
         $stmt = $this->conn->prepare($query);
         $stmt->execute();
         return $stmt;
